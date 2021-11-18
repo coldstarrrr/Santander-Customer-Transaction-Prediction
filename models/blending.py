38f64736689a8b8f74ce033a735fcd9176480c8e
coldstarrrr <wutong0218@ucla.edu>
2021-11-18 11:12:55 -0500

    add result for blending model

--- models/blending.py
+++ models/blending.py
@@ -15,3 +15,13 @@ if __name__ == "__main__":
     logit_submission = pd.read_csv(os.path.join(path, "logit_submission.csv"))
     # random forest
     rf_submission = pd.read_csv(os.path.join(path, "rf_submission.csv"))
+
+    # blending 1: 2 LGBM + 1 NN
+    blending_submission_1 = pd.DataFrame({"ID_code": nn_submission.ID_code, "target": (2 * lgbm_submission.target + nn_submission.target)/3})
+    blending_submission_1.to_csv(os.path.join(path, "blending_submission_1.csv"))
+    # blending 2: 3 LGBM + 2 NN + 1 Random Forest
+    blending_submission_2 = pd.DataFrame({"ID_code": nn_submission.ID_code, "target": (3 * lgbm_submission.target + 2 * nn_submission.target + rf_submission.target) / 6})
+    blending_submission_2.to_csv(os.path.join(path, "blending_submission_2.csv"))
+    # blending 3: 3 LGBM + 2 NN + 1 Random Forest + 1 Gaussian Naive Bayes
+    blending_submission_3 = pd.DataFrame({"ID_code": nn_submission.ID_code, "target": (3 * lgbm_submission.target + 2 * nn_submission.target + rf_submission.target + gnb_submission.target) / 7})
+    blending_submission_3.to_csv(os.path.join(path, "blending_submission_3.csv"))
